879c6321f1229b30945b7deb95a8b9afbb6abf2f
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-01-24 17:21:22 -0300

    renders PlanetCard list to SolarSystem.js

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -7,7 +7,7 @@ class PlanetCard extends Component {
     return (
       <div data-testid="planet-card">
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
-        <p>{ planetName }</p>
+        <p data-testid="planet-name">{ planetName }</p>
       </div>
     );
   }
