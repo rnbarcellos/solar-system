d96b8842f732ac317057567723c694e87842425b
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-01-24 17:31:28 -0300

    creates Missions and renders it to App.js

---
+++ src/components/Missions.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Missions extends Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        Conteúdo
+      </div>
+    );
+  }
+}
+
+export default Missions;
