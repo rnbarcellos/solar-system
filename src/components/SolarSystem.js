a3f0fef5e2e4ea5cba159540de2065d1aac4de02
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-01-24 15:14:49 -0300

    creates SolarSystem and renders it to App.js

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return <div data-testid="solar-system">Content</div>;
+  }
+}
+
+export default SolarSystem;
